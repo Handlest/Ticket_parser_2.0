f4bb6074e630ec7bf1282ffe809779d4087fbd48
Handlest <Eldoradozolotoe@gmail.com>
2023-05-16 23:54:07 +0700

    Change in project structure
    Remove db files
    Remove idea folder
    Add gitignore

--- charts.py
+++ charts.py
@@ -1,19 +1,41 @@
 import plotly.graph_objs as go
 import sqlite3
-
 connection = sqlite3.connect("tickets.db")
 cursor = connection.cursor()
-def build_chart():
-    cursor.execute('SELECT * FROM tickets WHERE gate = "Победа" ORDER BY depart_date')
-    records = cursor.fetchall()
-    print("Всего строк:  ", len(records))
-    prices = []
-    dates = []
-    for row in records:
-        prices.append(row[0])
-        dates.append(row[6])
+def company(company, fr='LED', to='MOW'):
+    try:
+        cursor.execute(f'SELECT value, depart_date'
+                       f' FROM tickets'
+                       f' WHERE gate = ? AND origin = "LED" AND destination = "MOW"'
+                       f' ORDER BY depart_date',
+                       (company, ))
+        records = list(zip(*cursor.fetchall()))
+        fig = go.Figure()
+        print("Всего строк подходящих под условие: ", len(records[0]))
+        fig.add_trace(go.Scatter(x=records[1], y=records[0], name='price', mode='lines+markers'))
+        fig.update_layout(title=f'Цены на авиабилеты компании {company} из {fr} в {to}', xaxis_title='День вылета',
+                          yaxis_title='Цена, руб', margin=dict(l=0, r=0, t=40, b=0))
+        fig.show()
+    except Exception as ex:
+        print(ex)
+
+def date_range(start, end, fr='LED', to='MOW'):
+    cursor.execute(f'SELECT value, depart_date'
+                   f' FROM tickets'
+                   f' WHERE depart_date BETWEEN ? AND ? AND origin = "LED" AND destination = "MOW"'
+                   f' ORDER BY depart_date',
+                   (start, end))
+    records = list(zip(*cursor.fetchall()))
+    print("Всего строк подходящих под условие: ", len(records[0]))
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=dates, y=prices))
+    fig.add_trace(go.Scatter(x=records[1], y=records[0], name='price', mode='lines+markers'))
+    fig.update_layout(title=f'Цены на авиабилеты из {fr} в {to} с {start} по {end}', xaxis_title='День вылета',
+                      yaxis_title='Цена, руб', margin=dict(l=0, r=0, t=40, b=0))
     fig.show()
 
-build_chart()
+
+
+
+
+
+date_range("2022-01-10", "2023-05-15", 'MOW', 'LED')
--- main.py
+++ main.py
@@ -5,16 +5,14 @@ import schedule
 import sqlite3
 import requests
 import pandas as pd
-
+from myapi import api
 
 connection = sqlite3.connect("tickets.db")
 cursor = connection.cursor()
 
-
-
-def get_month_prices():
-    URL = 'http://api.travelpayouts.com/v2/prices/month-matrix?currency=rub&origin=LED&destination=MOW' \
-          '&show_to_affiliates=true&token=e792af02824136cab4885087552040fc'
+def get_month_prices(fr='LED', to='MOW'):
+    URL = f'http://api.travelpayouts.com/v2/prices/month-matrix?currency=rub&origin={fr}&destination={to}' \
+          f'&show_to_affiliates=true&token={api}'
     response = requests.get(URL).json()
     if response['success']:
         df = pd.json_normalize(response, record_path='data')
@@ -23,21 +21,25 @@ def get_month_prices():
         with open('log.txt') as log_file:
             log_file.writelines(f'Ошибка при сборе информации. {response["error"]}. Время: {str(datetime.datetime.now())}')
 
-def parse_and_write():
-    data = get_month_prices()
+
+def parse_and_write(fr='LED', to='MOW'):
+    data = get_month_prices(fr, to)
     data.to_sql('tickets', connection, if_exists='append', index=False)
+    print("Актуальная информация по билетам успешно собрана и добавлена!")
+
 
 def main():
     cursor.execute(
         'CREATE TABLE IF NOT EXISTS tickets(value, trip_class, show_to_affiliates, origin, destination, gate,'
         'depart_date, return_date, number_of_changes, found_at, duration, distance, actual)')
-    parse_and_write()
+    parse_and_write('MOW', 'LED')
 
 
 if __name__ == '__main__':
-    schedule.every().monday.at("10:00").do(main)
-    schedule.every().wednesday.at("10:00").do(main)
-    schedule.every().friday.at("10:00").do(main)
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    main()
+    # schedule.every().monday.at("10:00").do(main)
+    # schedule.every().wednesday.at("10:00").do(main)
+    # schedule.every().friday.at("10:00").do(main)
+    # while True:
+    #     schedule.run_pending()
+    #     time.sleep(1)
